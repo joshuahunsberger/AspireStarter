e91b2cd372102fdb72a4e56a5fd27768c88704fc
Joshua Hunsberger <joshua@hunsberger.co>
2026-03-04 20:18:50 -0500

    Add service bus hosting integration

--- AspireStarter.AppHost/AppHost.cs
+++ AspireStarter.AppHost/AppHost.cs
@@ -2,6 +2,9 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddAzureContainerAppEnvironment("env");
 
+var serviceBus = builder.AddAzureServiceBus("messaging");
+var queue = serviceBus.AddServiceBusQueue("queue");
+
 var server = builder.AddProject<Projects.AspireStarter_Server>("server")
     .WithHttpHealthCheck("/health")
     .WithExternalHttpEndpoints();
