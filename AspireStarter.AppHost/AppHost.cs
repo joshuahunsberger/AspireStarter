844474d689e715ed038d368b5c01667a79567518
Joshua Hunsberger <joshua@hunsberger.co>
2026-03-04 20:23:10 -0500

    Add Aspire service bus client integration

--- AspireStarter.AppHost/AppHost.cs
+++ AspireStarter.AppHost/AppHost.cs
@@ -7,7 +7,8 @@ var queue = serviceBus.AddServiceBusQueue("queue");
 
 var server = builder.AddProject<Projects.AspireStarter_Server>("server")
     .WithHttpHealthCheck("/health")
-    .WithExternalHttpEndpoints();
+    .WithExternalHttpEndpoints()
+    .WithReference(queue);
 
 var webfrontend = builder.AddViteApp("webfrontend", "../frontend")
     .WithReference(server)
--- WorkerService/Program.cs
+++ WorkerService/Program.cs
@@ -1,6 +1,7 @@
 using WorkerService;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.AddAzureServiceBusClient(connectionName: "queue");
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
