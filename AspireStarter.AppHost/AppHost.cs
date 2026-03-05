0929c7d3a52310ef04a180d72d03e7a9ff406d0d
Joshua Hunsberger <joshua@hunsberger.co>
2026-03-04 22:18:54 -0500

    Add queue reference to consumer instead of web server

--- AspireStarter.AppHost/AppHost.cs
+++ AspireStarter.AppHost/AppHost.cs
@@ -9,8 +9,7 @@ var queue = serviceBus.AddServiceBusQueue("queue");
 
 var server = builder.AddProject<Projects.AspireStarter_Server>("server")
     .WithHttpHealthCheck("/health")
-    .WithExternalHttpEndpoints()
-    .WithReference(queue);
+    .WithExternalHttpEndpoints();
 
 var webfrontend = builder.AddViteApp("webfrontend", "../frontend")
     .WithReference(server)
@@ -18,6 +17,7 @@ var webfrontend = builder.AddViteApp("webfrontend", "../frontend")
 
 server.PublishWithContainerFiles(webfrontend, "wwwroot");
 
-var consumer = builder.AddProject<WorkerService>("queueconsumer");
+var consumer = builder.AddProject<WorkerService>("queueconsumer")
+    .WithReference(queue);
 
 builder.Build().Run();
