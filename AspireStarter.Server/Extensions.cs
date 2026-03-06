29560276a5d66c1cd789831c486c2d2deca91f86
Joshua Hunsberger <joshua@hunsberger.co>
2026-03-06 08:26:20 -0500

    Just share Otel setup

--- AspireStarter.Server/Extensions.cs
+++ AspireStarter.Server/Extensions.cs
@@ -57,7 +57,7 @@ public static class Extensions
                 );
             });
 
-        builder.AddServiceDefaults();
+        builder.ConfigureOpenTelemetry();
 
         return builder;
     }
