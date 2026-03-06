55b034d096442301fd260cbaf9196be21af63fb3
Joshua Hunsberger <joshua@hunsberger.co>
2026-03-06 08:35:47 -0500

    Add Azure as a Otel trace source

--- ServiceDefaults/Extensions.cs
+++ ServiceDefaults/Extensions.cs
@@ -51,6 +51,7 @@ public static class Extensions
             .WithTracing(tracing =>
             {
                 tracing.AddSource(builder.Environment.ApplicationName)
+                    .AddSource("Azure")
                     .AddHttpClientInstrumentation();
             });
 
